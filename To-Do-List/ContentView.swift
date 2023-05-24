a42c121a0e0416f07414ef1b245a12703e849f1a
DanielTroyano <troyano_daniel@icloud.com>
2023-05-24 19:45:05 -0400

    Initial Commit

---
+++ To-Do-List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  To-Do-List
+//
+//  Created by Daniel Troyano on 5/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
